90266a89bf7facef1116f4bd55b856e215db201e
Chandler Wright <S534776@nwmissouri.edu>
2020-09-27 12:07:29 -0500

    Added the function to calc triangle areas

---
+++ triangleCalc.js
@@ -0,0 +1,6 @@
+calcTriArea = () => {
+  var triBase = document.getElementById("baseText").value;
+  var triHeight = document.getElementById("heightText").value;
+  var answer = (triBase * triHeight) / 2;
+  document.getElementById("answer").innterHTML = area;
+};
