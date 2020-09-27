bac99d363411c47778f0843ef2aad2ed2787d49f
Chandler Wright <S534776@nwmissouri.edu>
2020-09-27 14:43:10 -0500

    Fixed spelling errors
    I also am printing the answer to the console

--- triangleCalc.js
+++ triangleCalc.js
@@ -2,5 +2,6 @@ function calcTriArea() {
   var triBase = document.getElementById("baseText").value;
   var triHeight = document.getElementById("heightText").value;
   var answer = (triBase * triHeight) / 2;
-  document.getElementById("answer").innterHTML = area;
+  document.getElementById("answer").innterHTML = answer;
+  console.log(answer);
 }
