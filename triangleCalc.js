5aa8217dd97585a2f1a0bfc00e908347f42a2d77
Chandler Wright <S534776@nwmissouri.edu>
2020-09-27 14:24:02 -0500

    Removed ; from the end of the function

--- triangleCalc.js
+++ triangleCalc.js
@@ -1,6 +1,6 @@
-calcTriArea(){
+calcTriArea()
   var triBase = document.getElementById("baseText").value;
   var triHeight = document.getElementById("heightText").value;
   var answer = (triBase * triHeight) / 2;
   document.getElementById("answer").innterHTML = area;
-};
+}
